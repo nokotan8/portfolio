ee13c6d201d186f6aba6a40bea1ebf4543abb938
fancywhale23 <justintaoau@gmail.com>
2025-06-08 19:32:55 +1000

    added dock and about me

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,77 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
+    import { base } from "$app/paths";
+    import { page } from "$app/state";
     import "../app.css";
+    import { fade_out, fade_in } from "$lib/fade.js";
+    import { page_state } from "$lib/state.svelte.js";
+
+    const is_on_page = (path: string): "dock-item-selected" | "" => {
+        if (path === "")
+            return page.url.pathname === `${base}/` ? "dock-item-selected" : "";
+        return page.url.pathname.includes(path) ? "dock-item-selected" : "";
+    };
+
+    const animate_dock = (): "dock-float-in" | "" => {
+        if (page_state.first_load) {
+            page_state.first_load = false;
+            return page.url.pathname === `${base}/` ? "dock-float-in" : "";
+        }
+        return "";
+    };
+
+    // If navigating to a different page on the same website,
+    // fade in and out everything except the dock.
+    const navigate = (e: Event, location: string) => {
+        if (location !== page.url.pathname) {
+            e.preventDefault();
+            const body_except_dock =
+                document.getElementById("body-except-dock");
+            if (body_except_dock) {
+                fade_out(body_except_dock, 20);
+                setTimeout(() => {
+                    goto(location);
+                }, 100);
+                setTimeout(() => {
+                    fade_in(body_except_dock, 20);
+                }, 100);
+            }
+        }
+    };
 
     let { children } = $props();
-
 </script>
 
-{@render children()}
+<div
+    class={"bg-dark-surface fixed bottom-0 left-0 z-50 w-full shadow-md backdrop-blur " +
+        animate_dock()}
+>
+    <div
+        class="text-dark-subtle mx-auto flex max-w-4xl items-center justify-around px-4 py-2"
+    >
+        <a
+            onclick={(e) => navigate(e, base + "/")}
+            href="{base}/"
+            class={"dock-item " + is_on_page("")}>About</a
+        >
+        <a
+            onclick={(e) => navigate(e, base + "/projects")}
+            href="{base}/projects"
+            class={"dock-item " + is_on_page("projects")}>Projects</a
+        >
+        <a
+            onclick={(e) => navigate(e, base + "/uni")}
+            href="{base}/uni"
+            class={"dock-item " + is_on_page("uni")}>Uni</a
+        >
+        <a
+            onclick={(e) => navigate(e, base + "/socials")}
+            href="{base}/socials"
+            class={"dock-item " + is_on_page("socials")}>Socials</a
+        >
+    </div>
+</div>
+
+<div id="body-except-dock">
+    {@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { page_state } from "$lib/state.svelte.js";
+    const animate_about_me = (item: string): string => {
+        if (page_state.first_load) {
+            if (item == "about-me") return "about-me-float-in";
+            else if (item == "name") return "name-float-in";
+            else return "";
+        }
+
+        return "";
+    };
+</script>
+
+<div id="about-me" class="text-dark-text mt-[25vh] flex flex-col gap-4">
+    <h1 class={"mb-3 text-5xl font-bold " + animate_about_me("name")}>
+        Hi, I'm Justin.
+    </h1>
+    <div class={"text-md flex flex-col gap-2 " + animate_about_me("about-me")}>
+        <p><span class="text-dark-rose font-bold">University:</span> UNSW</p>
+        <p>
+            <span class="text-dark-rose font-bold">Degree:</span> Computer Science
+        </p>
+        <p><span class="text-dark-rose font-bold">Interests:</span></p>
+        <ul class="flex flex-col gap-0.5 indent-5">
+            <li>Web application security</li>
+            <li>Concurrent / parallel computing</li>
+            <li>Cryptographic hashing</li>
+            <li>Computing algorithms</li>
+            <li>Games with parry mechanics</li>
+            <li>Manga</li>
+            <li>Badminton</li>
+        </ul>
+        I use Arch btw
+    </div>
+</div>
