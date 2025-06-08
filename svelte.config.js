bc10c26b6157ed67abba1d83301c845a9f757cf3
fancywhale23 <justintaoau@gmail.com>
2025-06-08 14:56:19 +1000

    setup

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    // Consult https://svelte.dev/docs/kit/integrations
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+
+    kit: {
+        appDir: "build",
+        adapter: adapter({
+            // fallback: "404.html"
+        }),
+        paths: {
+            base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
+        }
+    }
+};
+
+export default config;
